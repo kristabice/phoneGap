14e48432700a3db06f70297b99a83f364cc55775
Krista Bice <bubbybice@hotmail.com>
2018-04-24 19:17:21 -0600

    adding dynamic slider

--- Final App/platforms/browser/www/js/index.js
+++ Final App/platforms/browser/www/js/index.js
@@ -40,3 +40,34 @@ var app = {
 
     }
 };
+
+var week1 = "<h1>Week One</h1> <img src='img/Spirit Deer Fern.jpg' alt='quilt along'><p>We are on to block three and our second applique block, Deer Fern. Are you ready to jump in? Get your freezer paper and trace the templates.  I only traced one 1/4 of the wreath on the freezer paper.</p><p> I also didn't trace the circles. I used Perfect Circle™ mylar templates made by Karen Kay Buckley. You can order a set here. More on how to use them later.I started my wreath with a 18 inches square of green Grunge.</p><p> I then pressed it in half in both way so I had my center lines. Next I pressed the template in one quarter. Using the fold to line up the straight edges of the template. Then I used a pencil to trace that section. Repeat with the other quarter sections. </p>";
+
+var week2 = "<h1>Week Two</h1> <img src='img/Spirit Canoe.jpg' alt='quilt along'><p>We are on to block three and our second applique block, Deer Fern. Are you ready to jump in? Get your freezer paper and trace the templates.  I only traced one 1/4 of the wreath on the freezer paper.</p><p> I also didn't trace the circles. I used Perfect Circle™ mylar templates made by Karen Kay Buckley. You can order a set here. More on how to use them later.I started my wreath with a 18 inches square of green Grunge.</p><p> I then pressed it in half in both way so I had my center lines. Next I pressed the template in one quarter. Using the fold to line up the straight edges of the template. Then I used a pencil to trace that section. Repeat with the other quarter sections. </p>";
+
+var week3 = "<h1>Week Three</h1> <img src='img/Spirit Horses.jpg' alt='quilt along'><p>We are on to block three and our second applique block, Deer Fern. Are you ready to jump in? Get your freezer paper and trace the templates.  I only traced one 1/4 of the wreath on the freezer paper.</p><p> I also didn't trace the circles. I used Perfect Circle™ mylar templates made by Karen Kay Buckley. You can order a set here. More on how to use them later.I started my wreath with a 18 inches square of green Grunge.</p><p> I then pressed it in half in both way so I had my center lines. Next I pressed the template in one quarter. Using the fold to line up the straight edges of the template. Then I used a pencil to trace that section. Repeat with the other quarter sections. </p>";
+var week4 = "<h1>Week four</h1> <p>Comming Next Week!</p>";
+
+$(document).on('pagebeforeshow', '#page3', function(){ 
+    if($("#num").val() ==='1'){
+        $("#week").html(week1);
+    }else if($("#num").val() === '2'){
+        $("#week").html(week2);
+    }else if($("#num").val() === '3'){
+        $("#week").html(week3);
+    }else if($("#num").val() === '4'){
+        $("#week").html(week4);
+    }
+});
+
+    $(document).on('slidestop', '#page3', function(){ 
+    if($("#num").val() ==='1'){
+        $("#week").html(week1);
+    }else if($("#num").val() === '2'){
+        $("#week").html(week2);
+    }else if($("#num").val() === '3'){
+        $("#week").html(week3);
+    }else if($("#num").val() === '4'){
+        $("#week").html(week4);
+    }
+});
